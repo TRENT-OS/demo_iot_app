1d1e242906b9f4d438503f40bb23944ca1ae4924
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-07-27 11:42:06 +0200

    [REF] Rename OS_FileSystem_STORAGE_MAX
    OS_FileSystem_USE_STORAGE_MAX better describes the purpose of this variable,
    as it is used when max possibles size of the storage is selected for the files
    system.

--- components/LogServer/src/LogServer.c
+++ components/LogServer/src/LogServer.c
@@ -68,7 +68,7 @@ static OS_FileSystem_Handle_t hFs;
 static OS_FileSystem_Config_t cfgFs =
 {
     .type = OS_FileSystem_Type_FATFS,
-    .size = OS_FileSystem_STORAGE_MAX,
+    .size = OS_FileSystem_USE_STORAGE_MAX,
     .storage = IF_OS_STORAGE_ASSIGN(
         storage_rpc,
         storage_dp),
